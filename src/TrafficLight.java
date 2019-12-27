b7c3b5c9b5cbfc4bb1f84082e83560b30e954f22
DELL <minkhant.aung@my.jcu.edu.au>
2019-12-27 15:43:19 +0800

    updated traffic light class

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,16 +1,16 @@
 public class TrafficLight {
-    String color;
-    double rateOfChange;
+    String color; // the color of the traffic light
+    double rateOfChange; // variable to determine the traffic light change
 
-    public TrafficLight() {
+    public TrafficLight() { // default constructor
     }
 
     public TrafficLight(String color, double rateOfChange){
         this.color=color;
         this.rateOfChange=rateOfChange;
     }
-    public void operate(){
-        if(rateOfChange<(Math.random()*((1.0-10.0)+1))+10.0)
+    public void operate(){ // this method will change the color of traffic light
+        if(rateOfChange<(Math.random()*((1.0-10.0)+1))+10.0) // generate random numbers to determine colors
             this.color="Red";
         else
             this.color="Green";
