105e6717a00b398f5a39a0e4b13247a3a944ca40
DELL <minkhant.aung@my.jcu.edu.au>
2020-01-24 20:10:19 +0800

    added vehicleTest class

---
+++ src/VehicleTest.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class VehicleTest {
+    // Variables (package access)
+    int x, y, width, height; // Use an rectangle for illustration
+    Color color = Color.RED; // Color of the object
+
+    // Constructor
+    public VehicleTest(int x, int y, int width, int height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+}
